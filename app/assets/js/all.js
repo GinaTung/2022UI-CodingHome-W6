6767f59f708a4fdafdc8eaeb8923fbd1c11f8e6c
董聿凌 <gb2013429@gmail.com>
2022-08-28 00:50:51 +0800

    course+swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,23 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
