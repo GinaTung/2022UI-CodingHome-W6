5089894422549ac39bb4a9a534ada4a9ea24c6dd
董聿凌 <gb2013429@gmail.com>
2022-08-28 23:30:25 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,7 +9,14 @@ const swiper = new Swiper('.swiper', {
   fadeEffect: {
     crossFade: true
   },
-  
+  loop: true,
+  loopedSlides: 15,
+  grabCursor: true,
+  speed: 1500,
+  autoplay: {
+    delay: 0, 
+    waitForTransition: false 
+  },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
