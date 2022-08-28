ddd13c482ac2e8b3475d83bf06e4311738e88738
董聿凌 <gb2013429@gmail.com>
2022-08-29 00:46:01 +0800

    swipper-test

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,14 +9,6 @@ const swiper = new Swiper('.swiper', {
   fadeEffect: {
     crossFade: true
   },
-  loop: true,
-  loopedSlides: 15,
-  grabCursor: true,
-  speed: 1500,
-  autoplay: {
-    delay: 0, 
-    waitForTransition: false 
-  },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
@@ -28,3 +20,11 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const swiper2 = new Swiper('.swiper2', {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  
+})
